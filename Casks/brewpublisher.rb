3a5800bcc2514744e892ed92d3d5a46dcf5f6de1
Kevin <linxitj2003@icloud.com>
2025-12-02 15:32:57 +0800

    Update brewpublisher to 1.2 (Cask)

--- Casks/brewpublisher.rb
+++ Casks/brewpublisher.rb
@@ -1,8 +1,8 @@
 cask "brewpublisher" do
-  version "1.1.0"
-  sha256 "cdd2028fe5979cc7288a2046680d1d0a2495434612019e274170d66f70ed308d"
+  version "1.2"
+  sha256 "f467bd16954dd77d23c5afce7453fc85948ea36e2f0757d74d075414d101cf23"
 
-  url "https://github.com/chenyunguiMilook/BrewPublisher/releases/download/1.1.0/BrewPublisher.zip"
+  url "https://github.com/chenyunguiMilook/BrewPublisher/releases/download/1.2/BrewPublisher.zip"
   name "brewpublisher"
   desc ""
   homepage ""
