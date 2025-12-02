8d6b70a2c65fbb8f65348f0b65d029d7922cad96
Kevin <linxitj2003@icloud.com>
2025-12-02 17:02:50 +0800

    Update repomanager to 1.1 (Cask)

--- Casks/repomanager.rb
+++ Casks/repomanager.rb
@@ -1,30 +1,14 @@
 cask "repomanager" do
-  version "1.0.0"
-  # 替换为你上传到 Release 的 zip 文件的 SHA256
-  sha256 "c3b7a6e63243780a4068f394180051542e8649e23ef5af39f942211978b3b7a4"
+  version "1.1"
+  sha256 "94a462df70845faf533dda2ff7d31af4bf4cd8855951a08bbf886375cad143e9"
 
-  # 替换为你的 GitHub 仓库地址
-  url "https://github.com/chenyunguiMilook/RepoManager/releases/download/#{version}/RepoManager.zip"
+  url "https://github.com/chenyunguiMilook/RepoManager/releases/download/1.1/RepoManager.zip"
+  name "repomanager"
+  desc ""
+  homepage ""
 
-  name "RepoManager"
-  desc "A native macOS Git repository manager"
-  # 替换为你的项目主页
-  homepage "https://github.com/chenyunguiMilook/RepoManager"
+  auto_updates true
+  depends_on macos: ">= :monterey"
 
-  # 因为使用了 Window 和 SMAppService，建议限制系统版本
-  depends_on macos: ">= :tahoe"
-
-  # 告诉 brew 安装哪个 App
-  app "RepoManager.app"
-
-  # 可选：如果你希望在终端输入 `repo` 就能打开 App
-  # 注意：这可能会与 git repo 命令冲突，建议用 repomanager 或 rmgr
-  # binary "#{appdir}/RepoManager.app/Contents/MacOS/RepoManager", target: "repomanager"
-
-  # 卸载清理路径 (请根据你实际的 Bundle Identifier 修改 com.yourname...)
-  zap trash: [
-    "~/Library/Application Support/RepoManager",
-    "~/Library/Preferences/com.yourname.RepoManager.plist",
-    "~/Library/Saved Application State/com.yourname.RepoManager.savedState"
-  ]
+  app "repomanager.app"
 end
