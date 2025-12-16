dc443aefd9f6c8698df2777d3bb4fa4bdd36ed6d
Kevin <linxitj2003@icloud.com>
2025-12-16 21:03:27 +0800

    Update repomanager to 1.9 (Cask)

--- Casks/repomanager.rb
+++ Casks/repomanager.rb
@@ -1,8 +1,8 @@
 cask "repomanager" do
-  version "1.8"
-  sha256 "5c783ed1c7d120fc7e11c45d35dc317a9bac144daf5b7433e7539ecc1fc6e033"
+  version "1.9"
+  sha256 "4ec40ebd599bcb329e176f89d7ff79b507a302d719b65d7615c676dee3023670"
 
-  url "https://github.com/chenyunguiMilook/RepoManager/releases/download/1.8/RepoManager.zip"
+  url "https://github.com/chenyunguiMilook/RepoManager/releases/download/1.9/RepoManager.zip"
   name "repomanager"
   desc ""
   homepage ""
