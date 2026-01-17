edc2e345db1d5cc72708258c1514b6cc27792957
Kevin <linxitj2003@icloud.com>
2026-01-17 11:05:08 +0800

    Update swiftlens to 2.4 (Cask)

--- Casks/swiftlens.rb
+++ Casks/swiftlens.rb
@@ -1,8 +1,8 @@
 cask "swiftlens" do
-  version "1.0"
-  sha256 "b616b4fc922357306a3f8d358d614eed87d3d622cb6f6153607050c95136df58"
+  version "2.4"
+  sha256 "a2888af83f05d385e727ab057a5be2f86cc8a88ee625f8412b17deb6ccdad9ea"
 
-  url "https://github.com/chenyunguiMilook/SwiftLens/releases/download/1.0/SwiftLens.zip"
+  url "https://github.com/chenyunguiMilook/RepoManager/releases/download/2.4/RepoManager.zip"
   name "swiftlens"
   desc ""
   homepage ""
